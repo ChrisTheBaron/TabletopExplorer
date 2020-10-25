db822844246ef3d1c0bdd3fbdc5ff60e2f1be922
Chris Taylor <christhebaron@gmail.com>
2020-10-25 20:19:12 +0000

    Still proof of concept

---
+++ viewer/viewer.js
@@ -0,0 +1,75 @@
+import 'babel-polyfill'
+import Libp2p from 'libp2p'
+import Websockets from 'libp2p-websockets'
+import WebRTCStar from 'libp2p-webrtc-star'
+import { NOISE } from 'libp2p-noise'
+import Mplex from 'libp2p-mplex'
+import Gossipsub from 'libp2p-gossipsub'
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    const topic = 'tabletop-explorer';//TODO Get this from query string
+
+    // Create our libp2p node
+    const libp2p = await Libp2p.create({
+        addresses: {
+            // Add the signaling server address, along with our PeerId to our multiaddrs list
+            // libp2p will automatically attempt to dial to the signaling server so that it can
+            // receive inbound connections from other peers
+            listen: [
+                '/dns4/whispering-harbor-98265.herokuapp.com/tcp/443/wss/p2p-webrtc-star/'
+            ]
+        },
+        modules: {
+            transport: [Websockets, WebRTCStar],
+            connEncryption: [NOISE],
+            streamMuxer: [Mplex],
+            pubsub: Gossipsub
+        }
+    })
+
+    const gsub = new Gossipsub(libp2p);
+
+    let peers = [];
+
+    // Listen for new peers
+    libp2p.on('peer:discovery', async (peerId) => {
+        console.log(`Found peer ${peerId.toB58String()}`);
+    });
+
+    // Listen for new connections to peers
+    libp2p.connectionManager.on('peer:connect', (connection) => {
+        console.log(`Connected to ${connection.remotePeer.toB58String()}`);
+        peers.push(connection.remotePeer.toB58String());
+    });
+
+    // Listen for peers disconnecting
+    libp2p.connectionManager.on('peer:disconnect', (connection) => {
+        console.log(`Disconnected from ${connection.remotePeer.toB58String()}`);
+        peers = peers.filter(p => p != connection.remotePeer.toB58String());
+    });
+
+    await libp2p.start();
+    await gsub.start();
+
+    gsub.subscribe(topic);
+
+    console.log("Waiting 5 seconds before requesting first frame");
+    setTimeout(() => {
+        if (peers.length > 0) {
+            gsub.publish(topic, new TextEncoder().encode('REQUEST'));
+        } else {
+            console.log("No peers.Waiting 5 seconds before requesting first frame");
+            setTimeout(() => {
+                if (peers.length > 0) {
+                    gsub.publish(topic, new TextEncoder().encode('REQUEST'));
+                }
+            }, 5000);
+        }
+    }, 5000);
+
+    gsub.on(topic, (data) => {
+        console.log(new TextDecoder().decode(data.data));
+    })
+
+})
